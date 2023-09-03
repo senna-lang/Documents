384429f90779a675295527c4e585dfeb449968d3
senna-lang <asrsena3302@gmail.com>
2023-09-03 15:40:35 +0900

    blog.API作成

--- nextjs-myblog/nextjs-myblog/src/app/blog/page.tsx
+++ nextjs-myblog/nextjs-myblog/src/app/blog/page.tsx
@@ -1,12 +1,15 @@
+import { getAllArticles } from "@/blogAPI";
 import ArticleList from "../components/ArticleList";
 import Aside from "../components/Aside";
 import React from "react";
 
-const blog = () => {
+const blog = async() => {
+  const articles = await getAllArticles();
+
   return (
     <div className="md:flex">
       <section className="w-full md:w-2/3 flex flex-col items-center px-3">
-        <ArticleList />
+        <ArticleList articles={articles}/>
       </section>
       <section className="md:w-1/3 flex flex-col items-center px-3 md:pl-6">
         <Aside />
