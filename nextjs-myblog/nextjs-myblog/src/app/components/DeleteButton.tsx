4294ead87ab7de92d9fb2945e36fc0530237f4a0
senna-lang <asrsena3302@gmail.com>
2023-09-08 20:27:27 +0900

    記事削除機能実装

--- nextjs-myblog/nextjs-myblog/src/app/components/DeleteButton.tsx
+++ nextjs-myblog/nextjs-myblog/src/app/components/DeleteButton.tsx
@@ -11,14 +11,18 @@ type DeleteButtonProps = {
 const DeleteButton = ({ id }: DeleteButtonProps) => {
   const router = useRouter();
   const handleDelete = async () => {
-    await deleteArticle(id);
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
+    await fetch(`${API_URL}/api/${id}`, {
+      method: "DELETE",
+    });
+
     router.push("/blog");
     router.refresh;
   };
 
   return (
     <button
-      className="bg-red-500 hover:bg-red-600 rounded-md py-2 py-5 inline cursor-pointer"
+      className="bg-red-500 hover:bg-red-600 rounded-md px-2 py-5 inline cursor-pointer"
       onClick={handleDelete}
     >
       DeleteButton
