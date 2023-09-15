f86fe0b375d710f8b7a680f003d75cbcacd1c698
senna-lang <asrsena3302@gmail.com>
2023-09-16 08:20:43 +0900

    notion から記事データを取得するnotionAPI作成

--- nextjs-myblog/nextjs-myblog/src/app/blog/page.tsx
+++ nextjs-myblog/nextjs-myblog/src/app/blog/page.tsx
@@ -1,13 +1,26 @@
-import { getAllArticles } from "@/blogAPI";
+// "use client";
+
 import ArticleList from "../components/ArticleList";
 import Aside from "../components/Aside";
 import React from "react";
-import { supabase } from "@/utils/supabaseClient";
 
 const blog = async () => {
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  const res = await fetch(`${API_URL}/api/blog`, { cache: "no-store" });
-  const articles = await res.json();
+  const res2 = await fetch(`${API_URL}/api/blog`, { cache: "no-store" });
+  const articles = await res2.json();
+
+  const res = await fetch(`${API_URL}/api/notion`, { next: { revalidate: 10 } });
+  const posts = await res.json();
+  const metaData = posts.map((post:any) => {
+    const meta = {
+      id: post.properties.Name.title[0].plain_text,
+      description: post.properties.Description.rich_text[0].plain_text,
+      date: post.properties.Date.date.start,
+      slug: post.properties.Slug.rich_text[0].plain_text,
+    };
+    return meta;
+  });
+  console.log(metaData);
 
   return (
     <div className="h-auto xl:flex xl:mx-36">
--- nextjs-myblog/nextjs-myblog/src/app/layout.tsx
+++ nextjs-myblog/nextjs-myblog/src/app/layout.tsx
@@ -12,6 +12,8 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+  
   return (
     <html lang="ja">
       <head />
