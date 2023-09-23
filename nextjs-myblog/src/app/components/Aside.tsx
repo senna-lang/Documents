5489208c28e0da57a990109031755701cc453719
senna-lang <asrsena3302@gmail.com>
2023-09-23 17:52:27 +0900

    カテゴリにタグを紐付け

--- nextjs-myblog/src/app/components/Aside.tsx
+++ nextjs-myblog/src/app/components/Aside.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import ContactCard from "./ContactCard";
 
-const Aside = () => {
+const Aside = async () => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${API_URL}/api/notion`, { next: { revalidate: 10 } });
+  const posts = await res.json();
+
+  const getTags:string[] = posts.flatMap((post: any) => {
+    const getTag = (tags: any) => {
+      const allTags = tags.map((tag: any) => {
+        return tag.name;
+      });
+      return allTags;
+    };
+
+    const tags = getTag(post.properties.Tags.multi_select);
+    return tags;
+  });
+  const set = new Set(getTags);
+  const tagList:string[] = Array.from(set);
+  const slicedTagList = tagList.slice(0,4);
+  
+
   return (
     <aside className="w-full">
       <div className="bg-white border rounded p-4 mt-8 w-full">
         <h3 className="font-bold text-gray-900 mb-2">Category</h3>
         <ul className="text-gray-600 mt-2">
-          <li>
-            <a href="#">Technology</a>
-          </li>
-          <li>
-            <a href="#">Automotive</a>
-          </li>
-          <li>
-            <a href="#">Finance</a>
-          </li>
-          <li>
-            <a href="#">Sports</a>
-          </li>
+          {slicedTagList.map((tag) => (
+            <li key={tag}>
+              <a href={`/allposts/tag/${tag}/1`}>{tag}</a>
+            </li>
+          ))}
         </ul>
       </div>
       <ContactCard />
