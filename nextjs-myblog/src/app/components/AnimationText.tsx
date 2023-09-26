8ede2ae1f34cc555360e080f95e55875d45115cb
senna-lang <asrsena3302@gmail.com>
2023-09-26 23:22:18 +0900

    テキストアニメーション追加

---
+++ nextjs-myblog/src/app/components/AnimationText.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AnimationText = () => {
+  return (
+    <div className=" absolute right-0">
+      <div className="typing-effect1">
+        Hello 
+      </div>
+      <div className="typing-effect2">
+        Welcome to 
+      </div>
+      <div className="typing-effect3">
+        SENN'S BLOG
+      </div>
+    </div>
+  );
+};
+
+export default AnimationText;
--- nextjs-myblog/src/app/components/MainVisual.tsx
+++ nextjs-myblog/src/app/components/MainVisual.tsx
@@ -2,23 +2,22 @@
 
 import React from "react";
 import Image from "next/image";
-import { useRef, useLayoutEffect} from "react";
+import AnimationText from "./AnimationText";
+import { useRef, useLayoutEffect } from "react";
 import { gsap } from "gsap";
 
 const MainVisual = () => {
-   const textRef = useRef<HTMLDivElement | null>(null);
-   useLayoutEffect(() => {
-      gsap.to(textRef.current, {
-        opacity:1,
-        color: "black",
-        duration:2,
-      });
-    }, []);
+  // const textRef = useRef<HTMLDivElement | null>(null);
+  // useLayoutEffect(() => {
+  //   gsap.to(textRef.current, {
+  //     opacity: 1,
+  //     color: "black",
+  //     duration: 2,
+  //   });
+  // }, []);
   return (
-    <div className="h-[600px] relative mx-36" >
-      <div className=" absolute right-0 bottom-10" >
-        <h1 className=" text-7xl opacity-0" ref={textRef}>text animation</h1>
-      </div>
+    <div className="h-[600px] relative mx-36">
+      <AnimationText />
       <div className=" absolute left-0 top-10">
         <Image
           src={`https://source.unsplash.com/collection/1346951/1000x500?sig=23`}
