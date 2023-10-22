8ad685bd3cec434e9552b6d7d253411d252b49fb
senna-lang <asrsena3302@gmail.com>
2023-10-22 09:36:52 +0900

    [refactor] useRouter を Linkコンポーネントに置換

--- nextjs-myblog/src/app/components/ArticleCard.tsx
+++ nextjs-myblog/src/app/components/ArticleCard.tsx
@@ -1,9 +1,7 @@
-"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Article } from "@/types";
-import { useRouter } from "next/navigation";
 import { BiSolidPurchaseTagAlt } from "react-icons/bi";
 import { TfiTime } from "react-icons/tfi";
 
@@ -12,13 +10,15 @@ type ArticleCardProps = {
 };
 
 const ArticleCard = ({ article }: ArticleCardProps) => {
-  const router = useRouter();
   return (
     <article
       className="my-2 bg-white min-h-[500px] cursor-pointer hover:shadow-lg transition-transform duration-500 translate-y-2 hover:translate-y-[-2]"
       key={article.id}
     >
-      <div className="hover:opacity-75 relative before:content-[''] before:block before: pt-[56.25%]" onClick={() => router.push(`/articles/${article.slug}`)}>
+      <Link
+        href={`/articles/${article.slug}`}
+        className=" block hover:opacity-75 relative before:content-[''] before:block before: pt-[56.25%]"
+      >
         <Image
           src={
             article.thumb
@@ -31,7 +31,7 @@ const ArticleCard = ({ article }: ArticleCardProps) => {
           priority
           className=" absolute top-0 left-0 w-full h-full object-cover"
         />
-      </div>
+      </Link>
       <div className=" flex flex-col justify-start pt-6 px-6">
         <div className="flex">
           <div className="mt-1 mr-2">
@@ -50,27 +50,21 @@ const ArticleCard = ({ article }: ArticleCardProps) => {
           </div>
         </div>
 
-        <div
+        <Link
+          href={`/articles/${article.slug}`}
           className="text-slate-900 text-3xl font-bold min-h-[70px] hover:text-gray-700 pb-2"
-          onClick={() => router.push(`/articles/${article.slug}`)}
         >
           {article.id}
-        </div>
+        </Link>
 
-        <div
-          className="text-slate-900 pb-10 mt-2 h-[110px]"
-          onClick={() => router.push(`/articles/${article.slug}`)}
-        >
+        <Link href={`/articles/${article.slug}`} className="text-slate-900 pb-10 mt-2 h-[110px]">
           {article.description}
-          {/* {article.description.length > 70 ? article.description.substring(0, 70) + "..." : article.description} */}
-        </div>
+        </Link>
 
-        <div
-          className="text-pink-800 hover:text-black"
-          onClick={() => router.push(`/articles/${article.slug}`)}
-        >
+        <Link href={`/articles/${article.slug}`} className="text-pink-800 hover:text-black">
           続きを読む
-        </div>
+        </Link>
+
         <span className="flex justify-end mb-4">
           <span className="mr-1 mt-[3.7px] text-xs ">
             <TfiTime />
