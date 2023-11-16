d2bcb3d7a626781456f1fc8991432e2b095482bf
senna-lang <asrsena3302@gmail.com>
2023-11-16 20:14:36 +0900

    likesプロパティの更新

---
+++ src/app/api/likes/[id]/route.ts
@@ -0,0 +1,48 @@
+import { Client } from "@notionhq/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { getPage } from "@/lib/notion";
+
+const notionSecret = process.env.NOTION_TOKEN!;
+
+const notion = new Client({
+  auth: notionSecret,
+});
+
+export async function PATCH(req: NextApiRequest, res: NextApiResponse) {
+  const url = req.url as string;
+
+  const urlParts: string[] = new URL(url).pathname.split("/");
+
+  const page_id: string = urlParts[urlParts.length - 1];
+
+  getPage(page_id).then((result) => {
+    const currentLikes: number = result; 
+
+    const options = {
+      method: "PATCH",
+      url: `https://api.notion.com/v1/pages/${page_id}`,
+      headers: {
+        Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+        "Notion-Version": "2022-06-28",
+        "content-type": "application/json",
+      },
+      data: {
+        properties: {
+          Likes: {
+            number: currentLikes + 1,
+          },
+        },
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
+}
