7d1ca4235c0607672bc4263ffe0ff56062ae1d7f
senna-lang <asrsena3302@gmail.com>
2023-11-16 20:15:15 +0900

    いいね機能の実装

--- src/app/components/ArticleComments.tsx
+++ src/app/components/ArticleComments.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { updateLikes } from "@/lib/notion";
+import axios from "axios";
 
 type LikesProps = {
   likes: any;
@@ -8,14 +8,34 @@ type LikesProps = {
 };
 
 const ArticleComments = ({ likes, id }: LikesProps) => {
-  const handleLikes = () => {
-    updateLikes(id);
+  const updateLikes = (id: string) => {
+    axios.patch(`/api/likes/${id}`)
+      .then((response) => {
+        console.log("リクエストが成功しました:", response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.error("サーバーエラー:", error.response.status, error.response.data);
+        } else if (error.request) {
+          console.error(
+            "リクエストが送信されましたが、レスポンスがありませんでした:",
+            error.request
+          );
+        } else {
+          console.error("エラーが発生しました:", error.message);
+        }
+      });
   };
+
+  
 
   return (
     <div className=" mt-2 rounded-lg bg-white p-3">
       <div className=" bg-orange-400 rounded-lg">
-        <button className="text-xl m-4  text-white" onClick={() => handleLikes()}>
+        <button
+          className="text-xl m-4  text-white"
+          onClick={() => updateLikes(id)}
+        >
           Smash the 🧡 button
         </button>
       </div>
