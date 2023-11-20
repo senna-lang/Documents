089abfc725b83b51f85c94781e950d30e27254c3
senna-lang <asrsena3302@gmail.com>
2023-11-20 20:47:41 +0900

    useSWR実装

--- src/app/components/ArticleComments.tsx
+++ src/app/components/ArticleComments.tsx
@@ -1,53 +1,26 @@
 "use client";
 import React, { useState } from "react";
-import axios from "axios";
+import { useLike } from "@/hooks/useLike";
 
 type LikesProps = {
-  likes: any;
   id: string;
 };
 
-const ArticleComments = ({ likes, id }: LikesProps) => {
+const ArticleComments = ({ id }: LikesProps) => {
   const [isLiked, setIsLiked] = useState<boolean>(false);
-  const updateLikes = (id: string) => {
-    setIsLiked(true);
-    axios
-      .patch(`/api/likes/${id}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        console.log("リクエストが成功しました:", response.data);
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.error("サーバーエラー:", error.response.status, error.response.data);
-        } else if (error.request) {
-          console.error(
-            "リクエストが送信されましたが、レスポンスがありませんでした:",
-            error.request
-          );
-        } else {
-          console.error("エラーが発生しました:", error.message);
-        }
-      });
-  };
+  const { data, trigger, isMutating } = useLike(id);
+  console.log(data)
 
   return (
     <div className=" mt-2 rounded-lg bg-white p-3">
       <div className={isLiked ? "bg-orange-200 rounded-lg" : " bg-orange-400 rounded-lg"}>
-        <button
-          className="text-xl m-4  text-white"
-          onClick={() => updateLikes(id)}
-          disabled={isLiked}
-        >
+        <button className="text-xl m-4  text-white" onClick={() => trigger()} disabled={isLiked}>
           Smash the 🧡 button
         </button>
       </div>
 
       <div>
-        <p>Likes : {likes ? likes : 0}</p>
+        <p>Likes : {data ? data : 0}</p>
       </div>
     </div>
   );
