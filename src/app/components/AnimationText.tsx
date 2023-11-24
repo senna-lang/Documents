e5346a6263033b3aa363475bfd956ae9298dd4f0
senna-lang <asrsena3302@gmail.com>
2023-11-24 21:14:08 +0900

    レスポンシブ調整

--- src/app/components/AnimationText.tsx
+++ src/app/components/AnimationText.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const AnimationText = () => {
   return (
-    <div className=" mb-3 p-2 md:mx-[27.5px] 2xl:absolute 2xl:left-[900px] 2xl:mx-0">
-      <span className="typing-effect1 block text-[3rem] xl:text-[4.5rem]">Hello</span>
-      <span className="typing-effect2 block text-[3rem] xl:text-[4.5rem]">Welcome to</span>
-      <span className="typing-effect3 block text-[3rem] xl:text-[4.5rem]">SENNS BLOG.</span>
+    <div className="w-[320px] sm:w-[640px] lg:w-[800px] mb-12 p-2 2xl:absolute 2xl:left-[900px] 2xl:mx-0">
+      <span className="typing-effect1 block text-[3rem] lg:text-[4.5rem]">Hello</span>
+      <span className="typing-effect2 block text-[3rem] lg:text-[4.5rem]">Welcome to</span>
+      <span className="typing-effect3 block text-[3rem] lg:text-[4.5rem]">SENNS BLOG.</span>
     </div>
   );
 };
