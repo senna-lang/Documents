97967129d8a8232adbfd23a906a84df199f8b75a
senna-lang <asrsena3302@gmail.com>
2023-11-26 14:50:00 +0900

    コメント機能完成

--- src/app/components/CommentForm.tsx
+++ src/app/components/CommentForm.tsx
@@ -2,35 +2,29 @@
 import React, { useState } from "react";
 import { useComment } from "@/hooks/useComment";
 
-
 type CommentProps = {
   id: string;
 };
 
 const CommentForm = ({ id }: CommentProps) => {
   const [isLiked, setIsLiked] = useState<boolean>(false);
-  const [comment, setComment] = useState('');
-  const { trigger, isMutating } = useComment(id,comment);
+  const [comment, setComment] = useState("");
+  const { trigger, isMutating } = useComment(id, comment);
 
-  
-  const handleCommentChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
-   setComment(e.target.value)
-  }
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
 
-  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-   e.preventDefault();
-   trigger();
-   setComment('');
-  }
-
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    trigger();
+    setComment("");
+  };
 
   return (
     <div className=" mt-2 rounded-lg bg-white p-3">
       <form onSubmit={handleSubmit}>
-        <label>
-          コメント:
-          <textarea value={comment} onChange={handleCommentChange} />
-        </label>
+        <textarea value={comment} onChange={handleCommentChange} />
         <button type="submit">コメントを投稿</button>
       </form>
     </div>
