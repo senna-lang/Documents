f150ef4479029532ebc5f0b84656668be48733e5
senna-lang <asrsena3302@gmail.com>
2023-12-03 13:25:28 +0900

    コメントフォームのモーダル化に伴う変更

--- src/app/components/CommentForm.tsx
+++ src/app/components/CommentForm.tsx
@@ -1,15 +1,21 @@
 "use client";
 import React, { useState } from "react";
+import { useRecoilState } from "recoil";
 import { useComment } from "@/app/hooks/useComment";
+import { Modal, Button, Textarea } from "@mantine/core";
+import { useDisclosure} from "@mantine/hooks";
+import { notificationState } from "../atoms/notification";
 
 type CommentProps = {
   id: string;
 };
 
 const CommentForm = ({ id }: CommentProps) => {
-  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [notification, setNotification] = useRecoilState(notificationState);
+ 
   const [comment, setComment] = useState("");
-  const { trigger, isMutating } = useComment(id, comment);
+  const { trigger } = useComment(id, comment);
+  const [opened, { open, close }] = useDisclosure(false);
 
   const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setComment(e.target.value);
@@ -19,14 +25,26 @@ const CommentForm = ({ id }: CommentProps) => {
     e.preventDefault();
     trigger();
     setComment("");
+    setNotification(!notification);
+    close();
   };
 
   return (
-    <div className=" mt-2 rounded-lg bg-white p-3">
-      <form onSubmit={handleSubmit}>
-        <textarea value={comment} onChange={handleCommentChange} />
-        <button type="submit">コメントを投稿</button>
-      </form>
+    <div>
+      <Modal opened={opened} onClose={close} title="Authentication" size="lg" trapFocus={false}>
+        <form onSubmit={handleSubmit}>
+          <Textarea
+            label="Input label"
+            description="Input description"
+            placeholder="Input placeholder"
+            value={comment}
+            onChange={handleCommentChange}
+          />
+          <button type="submit">コメントを投稿</button>
+        </form>
+      </Modal>
+
+      <Button onClick={open}>Open modal</Button>
     </div>
   );
 };
--- src/app/hooks/useComment.ts
+++ src/app/hooks/useComment.ts
@@ -1,22 +1,26 @@
 import useSWRMutation from "swr/mutation";
 import axios from "axios";
+import { isMutatingState } from "@/app/atoms/isMutating";
+import { useRecoilState } from "recoil";
 
 const url = "/api/comments";
 
-const postFetcher = async (url: string,text:string) => {
-  const response = await axios.post(url,text);
-
+const postFetcher = async (url: string, text: string) => {
+  const response = await axios.post(url, text);
   return response.data;
 };
 
-export const useComment = (page_id: string,text:string) => {
+export const useComment = (page_id: string, text: string) => {
+  const [commentMutating, setCommentMutating] = useRecoilState(isMutatingState);
   const {
     trigger,
-    isMutating,
     error: mutateError,
-  } = useSWRMutation(page_id ? `${url}/${page_id}` : null,
-  (url) => postFetcher(url,text)
-  );
+    isMutating,
+  } = useSWRMutation(page_id ? `${url}/${page_id}` : null, (url) => postFetcher(url, text), {
+    onSuccess: () => {
+      setCommentMutating(false);
+    },
+  });
 
   return {
     trigger,
