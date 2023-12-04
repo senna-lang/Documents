e5afe188a4b6cf0c35527aa4a1a20f34523e75e9
senna-lang <asrsena3302@gmail.com>
2023-12-04 20:14:42 +0900

    レイアウト変更

--- src/app/components/ArticleLilkes.tsx
+++ src/app/components/ArticleLilkes.tsx
@@ -2,9 +2,9 @@
 import CommentForm from "./CommentForm";
 import React, { useState } from "react";
 import { useLike } from "@/app/hooks/useLike";
+import { useComment } from "../hooks/useComment";
 import { Button } from "@mantine/core";
 import { FcLike } from "react-icons/fc";
-
 
 type LikesProps = {
   id: string;
@@ -12,36 +12,48 @@ type LikesProps = {
 
 const ArticleComments = ({ id }: LikesProps) => {
   const [isLiked, setIsLiked] = useState<boolean>(false);
-  const { data, trigger, isMutating } = useLike(id);
+  const { data : likes, trigger, isMutating } = useLike(id);
+  const { data : comment } = useComment(id);
 
   return (
-    <div className="flex justify-between items-center mt-2 rounded-lg bg-white p-3">
-      <div>
-        <Button
-          variant="light"
-          color="indigo"
-          size="lg"
-          onClick={() =>
-            trigger(null, {
-              onSuccess: () => setIsLiked(true),
-            })
-          }
-          disabled={isLiked}
-          loading={isMutating}
-        >
-          {isLiked ? (
-            "Thank You !!"
-          ) : (
-            <>
-              <span className="mr-1">
-                <FcLike />
-              </span>
-              + 1 ?
-            </>
-          )}
-        </Button>
+    <div className="mt-2 rounded-lg bg-white p-5">
+      <div className="flex justify-between items-center">
+        <div className=" w-full mx-4">
+          <div className="flex justify-between">
+            <span>いいね</span>
+            <span>{likes}件</span>
+          </div>
+          <div>
+            <p>コメント</p>
+          </div>
+        </div>
+        <div>
+          <Button
+            variant="light"
+            color="indigo"
+            size="lg"
+            onClick={() =>
+              trigger(null, {
+                onSuccess: () => setIsLiked(true),
+              })
+            }
+            disabled={isLiked}
+            loading={isMutating}
+          >
+            {isLiked ? (
+              "Thank You !!"
+            ) : (
+              <>
+                <span className="mr-1">
+                  <FcLike />
+                </span>
+                + 1 ?
+              </>
+            )}
+          </Button>
+        </div>
       </div>
-      <div>
+      <div className="mt-2">
         <CommentForm id={id} />
       </div>
     </div>
