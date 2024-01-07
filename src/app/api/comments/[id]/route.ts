9668a3b5591940eff991844193450cfcbc687590
senna-lang <asrsena3302@gmail.com>
2024-01-07 18:09:11 +0900

    エラーハンドリング

--- src/app/api/comments/[id]/route.ts
+++ src/app/api/comments/[id]/route.ts
@@ -2,28 +2,31 @@ import { NextRequest, NextResponse } from "next/server";
 import { createComment } from "@/app/lib/notion";
 
 export async function POST(req: NextRequest) {
-  const url = req.url as string;
+  try {
+    const url = req.url as string;
 
-  const urlParts: string[] = new URL(url).pathname.split("/");
+    const urlParts: string[] = new URL(url).pathname.split("/");
 
-  const page_id: string = urlParts[urlParts.length - 1];
+    const page_id: string = urlParts[urlParts.length - 1];
 
-  const bodyText = await req.text();
+    const bodyText = await req.text();
 
-
-  const postRes = await createComment({
-    parent: {
-      page_id,
-    },
-    rich_text: [
-      {
-        text: {
-          content: bodyText,
-        },
+    const postRes = await createComment({
+      parent: {
+        page_id,
       },
-    ],
-  });
+      rich_text: [
+        {
+          text: {
+            content: bodyText,
+          },
+        },
+      ],
+    });
 
-  const response = NextResponse.json(postRes);
-  return response;
+    const response = NextResponse.json(postRes);
+    return response;
+  } catch (err) {
+    console.log("エラーが発生しました。", err);
+  }
 }
--- src/app/api/likes/[id]/route.ts
+++ src/app/api/likes/[id]/route.ts
@@ -2,37 +2,45 @@ import { getPage, updatePage } from "@/app/lib/notion";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const url = req.url as string;
+  try {
+    const url = req.url as string;
 
-  const urlParts: string[] = new URL(url).pathname.split("/");
+    const urlParts: string[] = new URL(url).pathname.split("/");
 
-  const page_id: string = urlParts[urlParts.length - 1];
+    const page_id: string = urlParts[urlParts.length - 1];
 
-  const result = await getPage(page_id);
-  const currentLikes = NextResponse.json(result);
+    const result = await getPage(page_id);
+    const currentLikes = NextResponse.json(result);
 
-  return currentLikes;
+    return currentLikes;
+  } catch (err) {
+    console.log("エラーが発生しました。", err);
+  }
 }
 
 export async function PATCH(req: NextRequest) {
-  const url = req.url as string;
+  try {
+    const url = req.url as string;
 
-  const urlParts: string[] = new URL(url).pathname.split("/");
+    const urlParts: string[] = new URL(url).pathname.split("/");
 
-  const page_id: string = urlParts[urlParts.length - 1];
+    const page_id: string = urlParts[urlParts.length - 1];
 
-  const result = await getPage(page_id);
-  const currentLikes = result;
+    const result = await getPage(page_id);
+    const currentLikes = result;
 
-  const postRes = await updatePage({
-    page_id,
-    properties: {
-      Likes: {
-        number: currentLikes + 1,
+    const postRes = await updatePage({
+      page_id,
+      properties: {
+        Likes: {
+          number: currentLikes + 1,
+        },
       },
-    },
-  });
+    });
 
-  const response = NextResponse.json(postRes);
-  return response;
+    const response = NextResponse.json(postRes);
+    return response;
+  } catch (err) {
+    console.log("エラーが発生しました。", err);
+  }
 }
