e5575b1a196f0c6ea65830997ff11a099f324817
senna-lang <asrsena3302@gmail.com>
2024-01-09 16:56:17 +0900

    型修正

--- src/app/lib/notion.ts
+++ src/app/lib/notion.ts
@@ -6,10 +6,9 @@ import type {
   UpdatePageParameters,
   CreateCommentParameters,
 } from "@notionhq/client/build/src/api-endpoints";
-import { Post, PropertyItemsResponse } from "@/app/types/types";
-import { PostDetail } from "@/app/types/types";
+import { PropertyItemsResponse } from "@/app/types/types";
 
-export const revalidate = 1800;
+export const revalidate = 60;
 
 const notionSecret = process.env.NOTION_TOKEN!;
 const notionDataBaseId = process.env.NOTION_DATABASE_ID!;
@@ -20,70 +19,58 @@ const notion = new Client({
 
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
-export const getAllPosts = cache(async ():Promise<Post[]|undefined> => {
-  try {
-    const posts = await notion.databases.query({
-      database_id: notionDataBaseId,
-      page_size: 100,
-      filter: {
-        property: "Published",
-        checkbox: {
-          equals: true,
-        },
+export const getAllPosts = cache(async () => {
+  const posts = await notion.databases.query({
+    database_id: notionDataBaseId,
+    page_size: 100,
+    filter: {
+      property: "Published",
+      checkbox: {
+        equals: true,
       },
-      sorts: [
-        {
-          property: "Date",
-          direction: "descending",
-        },
-      ],
-    });
-    const allPosts = posts.results;
-    return allPosts;
-  } catch (err) {
-    console.log("エラーが発生しました。", err);
-  }
+    },
+    sorts: [
+      {
+        property: "Date",
+        direction: "descending",
+      },
+    ],
+  });
+  const allPosts = posts.results;
+  return allPosts;
 });
 
-export const getPostDetail = cache(async (slug: string):Promise<PostDetail|undefined> => {
-  try {
-    const response = await notion.databases.query({
-      database_id: notionDataBaseId,
-      filter: {
-        property: "Slug",
-        formula: {
-          string: {
-            equals: slug,
-          },
+export const getPostDetail = cache(async (slug: string) => {
+  const response = await notion.databases.query({
+    database_id: notionDataBaseId,
+    filter: {
+      property: "Slug",
+      formula: {
+        string: {
+          equals: slug,
         },
       },
-    });
-    const page = response.results[0];
-    if (!page) {
-      notFound();
-    }
-
-    const mbBlocks = await n2m.pageToMarkdown(page.id);
-
-    const mbString = n2m.toMarkdownString(mbBlocks);
-    return {
-      page,
-      mbString,
-    };
-  } catch (err) {
-    console.log("エラーが発生しました。", err);
+    },
+  });
+  const page = response.results[0];
+  if (!page) {
+    notFound();
   }
+
+  const mbBlocks = await n2m.pageToMarkdown(page.id);
+
+  const mbString = n2m.toMarkdownString(mbBlocks);
+  return {
+    page,
+    mbString,
+  };
 });
 
-export const getPage = async (page_id: string):Promise<number|undefined> => {
-  try {
-    const propertyId = "vLhm";
-    const response = await notion.pages.properties.retrieve({ page_id, property_id: propertyId });
-    const likes = (response as PropertyItemsResponse).number;
-    return likes;
-  } catch (err) {
-    console.log("エラーが発生しました。", err);
-  }
+export const getPage = async (page_id: string) => {
+  const propertyId = "vLhm";
+  const response = await notion.pages.properties.retrieve({ page_id, property_id: propertyId });
+  const likes = (response as PropertyItemsResponse).number;
+  return likes;
 };
 
 export const updatePage = async (params: UpdatePageParameters) => {
