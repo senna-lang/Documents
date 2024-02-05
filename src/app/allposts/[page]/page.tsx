8f4f4f701060d1beaa3b9a4b4c34bdd3f7bd8e11
senna-lang <asrsena3302@gmail.com>
2024-02-05 18:54:42 +0900

    console.log削除

--- src/app/allposts/[page]/page.tsx
+++ src/app/allposts/[page]/page.tsx
@@ -3,10 +3,12 @@ import * as blog from "@/features/blog/components/index";
 import { createMetaData } from "@/common/utils/metaData";
 import { getAllPosts } from "@/common/lib/notion";
 
+
 const BlogPageList = async ({ params }: { params: { page: number } }) => {
   const posts = await getAllPosts();
   //記事を最新の６つまでに
   const numberOfPage = Math.floor(posts.length / 6) + (posts.length % 6 > 0 ? 1 : 0);
+  console.log(numberOfPage);
   const currentPage: number = params?.page;
   const startIndex = (currentPage - 1) * 6;
   const endIndex = startIndex + 6;
